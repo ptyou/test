bbd470254562d8b78f18d2c0930cbdac29105f2f
ptyou <479919367@qq.com>
2016-05-13 13:01:26 +0800

    Update list.h

--- list.h
+++ list.h
@@ -17,12 +17,13 @@ typedef struct LNode{
     struct LNode *next;
 }ListNode,*ListNodeh;
 
-void initList(ListNodeh *L){
+Status initList(ListNodeh *L){
     *L = (ListNodeh)malloc(sizeof(ListNode));
     (*L)->next = NULL;
+    return 1;
 }
 Status listLength(ListNodeh L){
-    int j=1;
+    int j=0;
     ListNodeh p=L->next;
     while(p){
         p = p->next;
@@ -33,15 +34,15 @@ Status listLength(ListNodeh L){
 
 Status insertList(ListNodeh L,int i,int data){//插入函数
     ListNodeh p;
-    int j=0;
-    p = L->next;
-    //将节点移动到要插入的位置i与i-1之间
-    while(p&&j<i-1){
+    int j=-1;    //i可能为0
+    p = L;
+    //将节点移动到要插入的位置i与i-1之间 i的大小可能为0
+    while(p && j<i-1){
         p = p->next;
         j++;
     }
-    if(!p||j>i-1){
-        return 0;
+    if(j!=i-1){
+        return 0; //插入出错
     }
     //生成一个新的节点用于保存
     ListNodeh s = (ListNodeh)malloc(sizeof(ListNode));
@@ -76,3 +77,36 @@ void DestroyList(ListNodeh *L){
         (*L) = q;
     }
 }
+void GetNode(ListNodeh L,int i, Datatype *x){
+    //将指针移动到i-1与i处
+    ListNodeh p;
+    p = L->next;
+    int j=0;
+    while(p && j<i-1)
+    {
+        p = p->next;
+        j++;
+    }
+    *x = p->data;
+}
+
+// #include<list.h>
+
+// int main(){
+//     ListNodeh L;
+//     int m,i,n,x ;
+//     i =initList(&L);
+//     printf("初始化%d\n", i);
+//     for (i=0;i<10;i++){
+//         n=insertList(L,i,i+1); //注意i的值变化
+//         printf("插入%d\n",n );
+//     }
+//     m = listLength(L);
+//     printf("链表长度%d\n ", m);
+//     for(i=0;i<listLength(L);i++)
+//       {
+//         GetNode(L,i,&x);
+//         printf("元素%d\n", x);
+//       }
+//     return 0;
+// }
